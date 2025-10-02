7f8cbd3aabf4748187891e03a9fd3fcb18cf96ec
colebrookson <cole.brookson@gmail.com>
2025-10-02 10:17:20 -0400

    changing function

--- R/visualising_sampling.R
+++ R/visualising_sampling.R
@@ -6,29 +6,31 @@ library(sf)
 library(rnaturalearth)
 library(patchwork)
 
-arha_db <- readRDS(here("data" ,"Project_ArHa_database_2025-09-25.rds"))
+arha_db <- readRDS(here("data", "Project_ArHa_database_2025-09-25.rds"))
 
 mollweide_crs <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"
 
-combined <- arha_db$pathogen |> 
-  left_join(arha_db$host, by = "host_record_id") |> 
-  drop_na(host_species) |> 
+combined <- arha_db$pathogen |>
+  left_join(arha_db$host, by = "host_record_id") |>
+  drop_na(host_species) |>
   filter(taxonomic_level == "species")
 
-combined_summary <- combined |> 
-  group_by(pathogen_species_cleaned, host_species, country, iso3c) |> 
-  summarise(number_detected = sum(number_of_hosts, na.rm = TRUE),
-            number_tested = sum(number_tested, na.rm = TRUE),
-            number_positive = sum(number_positive, na.rm = TRUE))
+combined_summary <- combined |>
+  group_by(pathogen_species_cleaned, host_species, country, iso3c) |>
+  summarise(
+    number_detected = sum(number_of_hosts, na.rm = TRUE),
+    number_tested = sum(number_tested, na.rm = TRUE),
+    number_positive = sum(number_positive, na.rm = TRUE)
+  )
 
-n_pathogens_in_host <- combined_summary |> 
+n_pathogens_in_host <- combined_summary |>
   filter(number_positive != 0) |>
-  group_by(host_species) |> 
+  group_by(host_species) |>
   summarise(n_pathogens_in_species = n_distinct(pathogen_species_cleaned))
 
-n_hosts_of_pathogen <- combined_summary |> 
+n_hosts_of_pathogen <- combined_summary |>
   filter(number_positive != 0) |>
-  group_by(pathogen_species_cleaned) |> 
+  group_by(pathogen_species_cleaned) |>
   summarise(n_hosts_of_pathogen = n_distinct(host_species))
 
 summary_information <- combined_summary |>
@@ -38,73 +40,94 @@ summary_information <- combined_summary |>
   mutate(n_host_pathogen_records = n()) |>
   ungroup()
 
-world <- ne_countries(scale = 10,  returnclass = "sv") |>
+world <- ne_countries(scale = 10, returnclass = "sv") |>
   project(y = mollweide_crs)
 
-map_summary_1 <- merge(world |> 
-                         select(adm0_a3),
-                       summary_information |>
-                         distinct(iso3c, n_host_pathogen_records),
-                       by.x = "adm0_a3",
-                       by.y = "iso3c",
-                       all.x = TRUE)
+map_summary_1 <- merge(
+  world |>
+    select(adm0_a3),
+  summary_information |>
+    distinct(iso3c, n_host_pathogen_records),
+  by.x = "adm0_a3",
+  by.y = "iso3c",
+  all.x = TRUE
+)
 
 map_summary_1 |>
   ggplot() +
   geom_spatvector(aes(fill = n_host_pathogen_records)) +
   scale_fill_viridis_c(trans = "log10") +
-  labs(fill = "Host-Pathogen records",
-       caption = "Map shows the number of records (distinct pathogen species within different hosts).") +
+  labs(
+    fill = "Host-Pathogen records",
+    caption = "Map shows the number of records (distinct pathogen species within different hosts)."
+  ) +
   theme_minimal() +
   theme(legend.position = "bottom")
 
 # Specific examples
-country_iso = "KOR"
+country_iso <- "KOR"
 
 map_sampling <- function(data = combined, country_iso = "KOR") {
-  
+  country_name <- as.character(unique(combined$country_raw[which(
+    combined$iso3c == country_iso
+  )]))
+  if (length(country_name) > 1) {
+    country_name <- country_name[2]
+  }
   data <- combined |>
     filter(iso3c == country_iso) |>
     filter(coordinate_resolution_processed != "country") |>
     group_by(latitude, longitude, pathogen_species_cleaned, host_species) |>
-    summarise(n_tested = sum(number_tested, na.rm = TRUE),
-              n_positive = sum(number_positive, na.rm = TRUE),
-              prop_positive = n_positive/n_tested)
-  
+    summarise(
+      n_tested = sum(number_tested, na.rm = TRUE),
+      n_positive = sum(number_positive, na.rm = TRUE),
+      prop_positive = n_positive / n_tested
+    )
+
   coords <- data |>
     vect(geom = c("longitude", "latitude"), crs = "EPSG:4326") |>
     project(y = mollweide_crs) |>
     crds()
-  
+
   data_jittered <- data |>
     ungroup() |>
     select(-longitude, -latitude) |>
     cbind(coords) |>
     group_by(x, y) |>
-    mutate(n_at_loc = n(),
-           point_index = row_number())|>
+    mutate(
+      n_at_loc = n(),
+      point_index = row_number()
+    ) |>
     ungroup() |>
-    mutate(jitter_radius = 10000, 
-           angle = (point_index - 1) * (2 * pi / n_at_loc),
-           final_x = if_else(n_at_loc > 1, x + cos(angle) * jitter_radius, x),
-           final_y = if_else(n_at_loc > 1, y + sin(angle) * jitter_radius, y)) |>
+    mutate(
+      jitter_radius = 10000,
+      angle = (point_index - 1) * (2 * pi / n_at_loc),
+      final_x = if_else(n_at_loc > 1, x + cos(angle) * jitter_radius, x),
+      final_y = if_else(n_at_loc > 1, y + sin(angle) * jitter_radius, y)
+    ) |>
     vect(geom = c("final_x", "final_y"), crs = mollweide_crs)
-  
+
   # Pathogen
-  
+
   pathogen_map <- ggplot() +
-    geom_spatvector(data = world |>
-                      filter(adm0_a3 == country_iso),
-                    lwd = 1) +
-    geom_spatvector(data = data_jittered,
-                    aes(colour = pathogen_species_cleaned, size = prop_positive)) +
+    geom_spatvector(
+      data = world |>
+        filter(adm0_a3 == country_iso),
+      lwd = 1
+    ) +
+    geom_spatvector(
+      data = data_jittered,
+      aes(colour = pathogen_species_cleaned, size = prop_positive)
+    ) +
     scale_size_continuous(name = "Proportion Positive", range = c(1, 5)) +
     scale_color_viridis_d(name = "Pathogen Species") +
-    labs(title = paste0("Sampling by pathogen in: ", country_iso),
-         caption = paste0("Overlain points have been jittered by 10,000 meters around the reported location.")) +
+    labs(
+      title = paste0("Sampling by pathogen in: ", country_name),
+      caption = paste0("Overlain points have been jittered by 10,000 meters around the reported location.")
+    ) +
     theme_minimal() +
     theme(legend.position = "bottom")
-  
+
   pathogen_barchart_tested <- data_jittered |>
     as_tibble() |>
     group_by(pathogen_species_cleaned, host_species) |>
@@ -116,44 +139,53 @@ map_sampling <- function(data = combined, country_iso = "KOR") {
     labs(x = "Total Tested", y = NULL, title = "N Tested") +
     theme_minimal() +
     theme(legend.position = "none")
-  
+
   pathogen_barchart_prevalence <- data_jittered |>
     as_tibble() |>
     group_by(pathogen_species_cleaned, host_species) |>
-    summarise(n_tested = sum(n_tested, na.rm = TRUE),
-              n_positive = sum(n_positive, na.rm = TRUE),
-              prop_positive = n_positive/n_tested,
-              .groups = "drop") |>
+    summarise(
+      n_tested = sum(n_tested, na.rm = TRUE),
+      n_positive = sum(n_positive, na.rm = TRUE),
+      prop_positive = n_positive / n_tested,
+      .groups = "drop"
+    ) |>
     filter(n_tested != 0) |>
-    ggplot(aes(x = prop_positive, y = pathogen_species_cleaned)) + 
-    geom_point(aes(colour = host_species, fill = host_species), 
-               shape = 21, size = 3,
-               position = position_jitter(width = 0, height = 0.2)) +
+    ggplot(aes(x = prop_positive, y = pathogen_species_cleaned)) +
+    geom_point(aes(colour = host_species, fill = host_species),
+      shape = 21, size = 3,
+      position = position_jitter(width = 0, height = 0.2)
+    ) +
     scale_color_viridis_d(name = "Host Species") +
     scale_fill_viridis_d(name = "Host Species") +
     labs(x = "Pathogen prevalence", y = NULL, title = "Prevalence") +
     theme_minimal()
-  
+
   pathogen_dashboard <- pathogen_map + pathogen_barchart_tested + pathogen_barchart_prevalence +
-    plot_layout(widths = c(2, 1, 1),
-                guides = "collect") +
-    plot_annotation(title = paste0("Pathogen Surveillance Summary for: ", country_iso)) & 
+    plot_layout(
+      widths = c(2, 1, 1),
+      guides = "collect"
+    ) +
+    plot_annotation(title = paste0("Pathogen Surveillance Summary for: ", country_name)) &
     theme(legend.position = "bottom")
-  
+
   # Host
-  
+
   host_map <- ggplot() +
-    geom_spatvector(data = world |>
-                      filter(adm0_a3 == country_iso),
-                    lwd = 1) +
-    geom_spatvector(data = data_jittered,
-                    aes(colour = host_species, size = prop_positive)) +
+    geom_spatvector(
+      data = world |>
+        filter(adm0_a3 == country_iso),
+      lwd = 1
+    ) +
+    geom_spatvector(
+      data = data_jittered,
+      aes(colour = host_species, size = prop_positive)
+    ) +
     scale_size_continuous(name = "Proportion Positive", range = c(1, 4)) +
     scale_color_viridis_d(name = "Host Species") +
-    labs(title = paste0("Sampling by host in: ", country_iso)) +
+    labs(title = paste0("Sampling by host in: ", country_name)) +
     theme_minimal() +
     theme(legend.position = "bottom")
-  
+
   host_barchart_tested <- data_jittered |>
     as_tibble() |>
     group_by(pathogen_species_cleaned, host_species) |>
@@ -165,11 +197,11 @@ map_sampling <- function(data = combined, country_iso = "KOR") {
     labs(x = "Total Tested", y = NULL, title = "N Tested") +
     theme_minimal() +
     theme(legend.position = "none")
-  
+
   prevalence_raw_data <- data_jittered |>
     as_tibble() |>
     filter(n_tested != 0)
-  
+
   prevalence_summary_data <- prevalence_raw_data |>
     group_by(pathogen_species_cleaned, host_species) |>
     summarise(
@@ -178,38 +210,50 @@ map_sampling <- function(data = combined, country_iso = "KOR") {
       prop_positive = n_positive / n_tested,
       .groups = "drop"
     )
-  
-  host_barchart_prevalence <- ggplot(data = prevalence_raw_data, 
-                                     aes(y = host_species, colour = pathogen_species_cleaned)) +
+
+  host_barchart_prevalence <- ggplot(
+    data = prevalence_raw_data,
+    aes(y = host_species, colour = pathogen_species_cleaned)
+  ) +
     geom_point(aes(x = prop_positive),
-               position = position_jitter(width = 0, height = 0.3),
-               alpha = 0.6,
-               size = 2.5) +
-    geom_point(data = prevalence_summary_data,
-               aes(x = prop_positive, fill = pathogen_species_cleaned),
-               shape = 21,
-               size = 4,
-               stroke = 1.2) +
+      position = position_jitter(width = 0, height = 0.3),
+      alpha = 0.6,
+      size = 2.5
+    ) +
+    geom_point(
+      data = prevalence_summary_data,
+      aes(x = prop_positive, fill = pathogen_species_cleaned),
+      shape = 21,
+      size = 4,
+      stroke = 1.2
+    ) +
     scale_x_continuous(labels = scales::percent) +
     scale_colour_viridis_d(name = "Pathogen Species") +
     scale_fill_viridis_d(name = "Pathogen Species") +
     guides(color = guide_legend(override.aes = list(size = 4))) +
-    labs(x = "Pathogen Prevalence", 
-         y = NULL, 
-         title = "Prevalence",
-         caption = "Large circles represent the overall prevalence per host-pathogen pair.") +
+    labs(
+      x = "Pathogen Prevalence",
+      y = NULL,
+      title = "Prevalence",
+      caption = "Large circles represent the overall prevalence per host-pathogen pair."
+    ) +
     theme_minimal()
-  
+
   host_dashboard <- host_map + host_barchart_tested + host_barchart_prevalence +
-    plot_layout(widths = c(2, 1, 1),
-                guides = "collect") +
-    plot_annotation(title = paste0("Host Surveillance Summary for: ", country_iso)) & 
+    plot_layout(
+      widths = c(2, 1, 1),
+      guides = "collect"
+    ) +
+    plot_annotation(title = paste0("Host Surveillance Summary for: ", country_name)) &
     theme(legend.position = "bottom")
-  
-  return(list(pathogen_dashboard,
-              host_dashboard))
+
+  return(list(
+    pathogen_dashboard,
+    host_dashboard
+  ))
 }
 
 map_sampling(data = combined, country_iso = "BEL")
 map_sampling(data = combined, country_iso = "KOR")
 map_sampling(data = combined, country_iso = "ALB")
+map_sampling(data = combined, country_iso = "ARG")
--- southkorea.jl
+++ southkorea.jl
@@ -14,23 +14,30 @@ occ = GBIF.download(doi)
 species = ["Mus musculus", "Myodes regulus", "Crocidura lasiura", "Apodemus agrarius"]
 
 # Remove records without coordinates
-filter!(x->!ismissing(place(x)), elements(occ))
+filter!(x -> !ismissing(place(x)), elements(occ))
 
 # Remove the point in the pacific
-filter!(x->longitudes(x) > 100, elements(occ))
+filter!(x -> longitudes(x) > 100, elements(occ))
 
 
-occurrence_records = [Occurrences(filter(x->startswith(s)(entity(x)), elements(occ))) for s in species]
+occurrence_records =
+    [Occurrences(filter(x -> startswith(s)(entity(x)), elements(occ))) for s in species]
 
+# Get a polygon for South Korea
+countr = getpolygon(PolygonData(NaturalEarth, Countries))
+sk = countr["South Korea"]
 
 # Visualization of the first species occurrences
 poly(sk)
-scatter!(occurrence_records[begin], color=:red, markersize=3)
+scatter!(occurrence_records[begin], color = :red, markersize = 3)
 current_figure()
 
 
 # Download CHELSA2 BioClim Layers
-L = [Float32.(SDMLayer(RasterData(CHELSA2, BioClim)); layer=i, SDT.boundingbox(sk)...) for i in 1:19]
+L = [
+    Float32.(SDMLayer(RasterData(CHELSA2, BioClim)); layer = i, SDT.boundingbox(sk)...)
+    for i = 1:19
+]
 
 # Mask layers to South Korea's Land
 mask!(L, sk)
@@ -49,40 +56,40 @@ function fit_sdm(sp_idx)
     bag_sdm = Bagging(sdm, 30)
     train!(bag_sdm)
     return bag_sdm
-end 
+end
 
 # Fit SDMs (2 is omitted due to lack of presences)
-sdms = fit_sdm.([1,3,4])
+sdms = fit_sdm.([1, 3, 4])
 
 
 # lightweight variable importance metric
-vimp = variableimportance(sdms[1], kfold(sdms[1], k=2))
+vimp = variableimportance(sdms[1], kfold(sdms[1], k = 2))
 
 
 # Weights uncertainty with more focus toward high values (i.e. species is likely present)
 # (This is no longer used)
-weight_uncertainty(x) = (minimum(x) + 4*Statistics.median(x) + maximum(x)) / 6
+weight_uncertainty(x) = (minimum(x) + 4 * Statistics.median(x) + maximum(x)) / 6
 
 # Predict SDMs
 pred = [predict(sdm, L) for sdm in sdms]
 
 # Compute uncertainty as bootstrap variance (measured as the range between the 25% and 75% quantiles, iqr)
-unc = [predict(sdm, L, consensus=iqr) for sdm in sdms]
+unc = [predict(sdm, L, consensus = iqr) for sdm in sdms]
 #unc = [predict(sdm, L, consensus=weight_uncertainty) for sdm in sdms]
 
 
 # Randomly weight importance of each species
 r = rand(3)
 r = r ./ sum(r)
-weighted_pred = [r[i]*pred[i] for i in eachindex(pred)]
-weighted_unc = [r[i]*unc[i] for i in eachindex(unc)]
+weighted_pred = [r[i] * pred[i] for i in eachindex(pred)]
+weighted_unc = [r[i] * unc[i] for i in eachindex(unc)]
 
 
 # Compute the total prediction score
 total_pred = sum(weighted_pred)
 
 # Compute the total uncertainty
-total_unc = mosaic(var, weighted_unc) 
+total_unc = mosaic(var, weighted_unc)
 
 
 # Split into quadrants 
@@ -92,16 +99,13 @@ unc_cutoff = quantile(total_unc, [q])[begin]
 
 A = total_pred .> pred_cutoff
 B = total_unc .> unc_cutoff
-bon = sample(BalancedAcceptance(), total_unc, inclusion=total_unc)
+bon = sample(BalancedAcceptance(), total_unc, inclusion = total_unc)
 
-heatmap(nodata(A & B, false), colormap=["#b48ead"], label="Active Surv.")
-heatmap!(nodata(.!A & .!B, false), colormap=["#5e81ac"], label="Confirmation")
-heatmap!(nodata(.!A & B, false), colormap=["#a3be8c"], label="Discovery")
-heatmap!(nodata(A & .!B, false), colormap=["#d08770"], label="Passive Surv.")
+heatmap(nodata(A & B, false), colormap = ["#b48ead"], label = "Active Surv.")
+heatmap!(nodata(.!A & .!B, false), colormap = ["#5e81ac"], label = "Confirmation")
+heatmap!(nodata(.!A & B, false), colormap = ["#a3be8c"], label = "Discovery")
+heatmap!(nodata(A & .!B, false), colormap = ["#d08770"], label = "Passive Surv.")
 axislegend()
-scatter!([n for n in bon.nodes], color=:black)
+scatter!([n for n in bon.nodes], color = :black)
 
 current_figure()
-
-
-
